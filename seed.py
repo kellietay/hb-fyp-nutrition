6d39d78dcc8ea18cfc30dd8c76a4d2b3fe06de93
kellie <tayqianting@gmail.com>
2018-09-17 17:06:38 +0000

    Completed Seed.py for psql tables dietaryreference and caloriereference

---
+++ seed.py
@@ -0,0 +1,89 @@
+from sqlalchemy import func
+from model import User, Profile, Reference, Food, Record, Calorie, connect_to_db, db
+from server import app
+import datetime
+
+REFLIST = []
+
+def load_dietaryreference():
+    Reference.query.delete()
+
+    for row in open("seed_data/u.reference.csv"):
+        row = row.rstrip().split(",")
+
+        if len(row) == 28:
+
+            # gender, min_age, max_age, calories, carbohydrates, fiber, fat, protein, vitA, vitC, vitD, vitE, vitB6, vitB12, thiamin, riboflavin, niacin, folate, calcium, copper, iodine, iron, magnesium, phosphorus, selenium, zinc, potassium, sodium, chloride = row.split(",")
+
+            for i in range(0,28):
+                if row[i] == "":
+                    row[i] = None
+
+            print(row)
+
+            reference = Reference(min_age = row[1],
+                gender = row[0],
+                max_age = row[2],
+                carbohydrates = row[3],
+                fiber = row[4],
+                fat = row[5],
+                protein = row[6],
+                vitA = row[7],
+                vitC = row[8],
+                vitD = row[9],
+                vitE = row[10],
+                vitB6 = row[11],
+                vitB12 = row[12],
+                thiamin = row[13],
+                riboflavin = row[14],
+                niacin = row[15],
+                folate = row[16],
+                calcium = row[17],
+                copper = row[18],
+                iodine = row[19],
+                iron = row[20],
+                magnesium = row[21],
+                phosphorus = row[22],
+                selenium = row[23],
+                zinc = row[24],
+                potassium = row[25],
+                sodium = row[26],
+                chloride = row[27])
+
+            db.session.add(reference)
+
+        else:
+            print('error, row only has {} for gender {}, min_age {}'.format(len(row), row[0], row[1]))
+            break
+
+    db.session.commit()
+
+
+def load_dietaryreference():
+    Reference.query.delete()
+
+    for row in open("seed_data/u.calorie.csv"):
+        row = row.rstrip().split(",")
+
+        for i in range(0,len(row)):
+            if row[i] == "":
+                row[i] = None
+
+        calorie = Calorie(gender=row[0],
+            min_age=row[1],
+            max_age=row[2],
+            calories_sedentary=row[3],
+            calories_modactive=row[4],
+            calories_active=row[5])
+
+        db.session.add(calorie)
+
+    db.session.commit()
+
+
+
+if __name__ == "__main__":
+    connect_to_db(app)
+
+    # Import different types of data
+    load_dietaryreference()
---
+++ server.py
@@ -0,0 +1,22 @@
+"""Movie Ratings."""
+
+from jinja2 import StrictUndefined
+
+from flask import (Flask, render_template, redirect, request, flash,
+                   session)
+from flask_debugtoolbar import DebugToolbarExtension
+
+from model import User, Profile, Reference, Food, Record, Calorie, connect_to_db, db
+
+from sqlalchemy.sql import func
+
+
+app = Flask(__name__)
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
+
+# Normally, if you use an undefined variable in Jinja2, it fails
+# silently. This is horrible. Fix this so that, instead, it raises an
+# error.
+app.jinja_env.undefined = StrictUndefined
